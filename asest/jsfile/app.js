a6e22aff5cba513a94172eb6fdc967aba7b24e5c
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-08-19 22:07:48 +0600

    24%

--- asest/jsfile/app.js
+++ asest/jsfile/app.js
@@ -1,4 +1,4 @@
-// =======================toggle btn 
+//1. =======================toggle btn 
 const toogleOn = document.querySelector(".toggleOn");
 const toogleOff = document.querySelector(".toggleOff");
 
@@ -12,7 +12,7 @@ toogleOn.addEventListener("click", () => {
 });
 
 
-// ====================navigation 
+//2. ====================navigation 
 const responsiveNav = document.querySelector("#responsive-nav");
 const menu = document.querySelector(".menu");
 const closeMenu = document.querySelector(".closeBtn");
@@ -22,4 +22,38 @@ menu.addEventListener("click", () => {
 });
 closeMenu.addEventListener("click", () => {
      responsiveNav.style.right = "-100%";
+});
+
+
+//3. ===============curriculum faq 
+ const curFaq = document.querySelectorAll('.cur-content');
+ const Faq = document.querySelectorAll('.faq-content');
+
+curFaq.forEach( accs => {
+    accs.addEventListener("click", () => {
+        accs.classList.toggle("cf-active");
+    })
+});
+Faq.forEach( faq => {
+     faq.addEventListener('click', () => {
+          faq.classList.toggle("f-active");
+     })
+});
+
+//4. -------------curriculum img 
+const curTitle = document.querySelectorAll('.title');
+const curImg = document.querySelectorAll('.cur-img');
+
+curTitle.forEach( (title, index) => {
+     title.addEventListener('click', () => {
+          curTitle.forEach (title => {
+               title.classList.remove('ci-active')
+          });
+          title.classList.add ('ci-active');
+
+          curImg.forEach(content => {
+               content.classList.remove('ci-active');
+          });
+          curImg[index].classList.add('ci-active');
+     });
 });
